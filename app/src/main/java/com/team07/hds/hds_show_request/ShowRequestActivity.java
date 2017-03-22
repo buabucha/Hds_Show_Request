7c1ee3168a16bdef103e36c1cd419f99b3ca1f7d
Beam123 <bubble_beam08@hotmail.com>
2017-03-22 18:55:23 +0700

    Initial commit

---
+++ app/src/main/java/com/team07/hds/hds_show_request/ShowRequestActivity.java
@@ -0,0 +1,13 @@
+package com.team07.hds.hds_show_request;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_request);
+    }
+}
